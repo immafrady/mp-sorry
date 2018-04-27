47c61201447403f98c08c633b02fe30a6696a11b
Frady <immafrady@gmail.com>
2018-04-27 11:04:25 +0800

    项目启动

---
+++ utils/baseDemo.js
@@ -0,0 +1,55 @@
+module.exports = {
+    sorry: {
+        src: "",
+        [
+            {
+                title: "第一句",
+                default: "好啊"
+            },
+            {
+                title: "第二句",
+                default: "就算你是一流工程师"
+            },
+            {
+                title: "第三句",
+                default: "就算你出报告再完美"
+            },
+            {
+                title: "第四句",
+                default: "我叫你改报告你就要改"
+            },
+            {
+                title: "第五句",
+                default: "毕竟我是客户"
+            },
+            {
+                title: "第六句",
+                default: "客户了不起啊"
+            },
+            {
+                title: "第七句",
+                default: "sorry 客户真的了不起"
+            },
+            {
+                title: "第八句",
+                default: "以后叫他天天改报告"
+            },
+            {
+                title: "第九句",
+                default: "天天改 天天改"
+            },
+            {
+                title: "左",
+                default: "客户B"
+            },
+            {
+                title: "右",
+                default: "客户A"
+            },
+            {
+                title: "另一侧",
+                default: "工程师"
+            }
+        ]
+    }
+}
